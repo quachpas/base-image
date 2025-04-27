bf51e72d0f7cd4a8dbee898d2e57c01b24b0dbda
James Cole <james@firefly-iii.org>
2025-04-27 17:30:42 +0200

    Update scripts and add comments.

--- generate-docker-files.php
+++ generate-docker-files.php
@@ -2,7 +2,7 @@
 declare(strict_types=1);
 
 /**
- * Small script to generate custom Docker image.
+ * Small script to generate two custom Dockerfiles.
  *
  * Steps:
  *
@@ -11,20 +11,26 @@ declare(strict_types=1);
  */
 
 
+// loop over both types.
 $types = ['web', 'cli'];
 
 foreach ($types as $type) {
 
-    // copy original file
-    copy(sprintf('%s/Dockerfile-%s.original', __DIR__, $type), sprintf('%s/Dockerfile.%s', __DIR__, $type));
-    $filename = sprintf('%s/Dockerfile.%s', __DIR__, $type);
-    $content  = file_get_contents($filename);
-    $counter  = trim(file_get_contents('counter.txt'));
-    $date     = trim(file_get_contents('date.txt'));
+    // First, copy the original file to the destination file.
+    $originalFilename = sprintf('%s/Dockerfile-%s.original', __DIR__, $type);
+    $filename         = sprintf('%s/Dockerfile.%s', __DIR__, $type);
+    copy($originalFilename, $filename);
 
+    // get the content of the file and the content of the counter and date.
+    $content = file_get_contents($filename);
+    $counter = trim(file_get_contents('counter.txt'));
+    $date    = trim(file_get_contents('date.txt'));
+
+    // replace both in the content of the Dockerfile.
     $content = str_replace('%BASE_IMAGE_BUILD_NO%', $counter, $content);
     $content = str_replace('%BASE_IMAGE_BUILD_DATE%', $date, $content);
 
+    // write the content to the file.
     file_put_contents($filename, $content);
     debugMessage(sprintf('Added build date and time for image type %s in file %s.', $type, $filename));
 
